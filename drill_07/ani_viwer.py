e33c27fc2f73254621d5050f4e1fa5f34dd85e4a
HyunjungWoo <hjhs0628@naver.com>
2022-09-27 04:23:33 +0900

    drill_07
    2020182024 우현정

--- drill_07/ani_viwer.py
+++ drill_07/ani_viwer.py
@@ -1,19 +1,19 @@
 from pico2d import *
 open_canvas()
 grass = load_image('grass.png')
-character = load_image('ch3.png')
+character = load_image('ch2_jumping.png')
+
 
 x = 0
 frame = 0
 while(x < 800):
     clear_canvas()
     grass.draw(400,30)
-    character.clip_draw(frame * 120, 0,80,140,x,90) #왼쪽 아래 너비 높이
-
+    character.clip_draw(frame * 70,0, 74,66,x,90) #왼쪽 아래 너비 높이
     update_canvas()
-    frame = (frame + 1) % 5
+    frame = (frame + 1) % 8
     x += 5
-    delay(0.1)
+    delay(0.01)
     get_events()
 
 
